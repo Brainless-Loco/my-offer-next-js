0a45ec05d18bbf6c537688c168b466945b111760
Tonmoy Das <dev.tcdas@gmail.com>
2023-06-12 20:35:00 +0600

    username and email verification added

--- app/authenticate/page.js
+++ app/authenticate/page.js
@@ -1,14 +1,17 @@
 "use client"
-
 import React, { useEffect, useState } from 'react';
 import { Box, Container, Checkbox, FormControlLabel, Grid, TextField, Typography, RadioGroup, FormLabel, Radio, FormControl, Button } from '@mui/material';
 import './authenticate.css'
 import { auth, db } from '@/firebase/firebaseConfig';
-import { collection, getDocs, query, where } from "firebase/firestore/lite";
+import { addDoc, collection, doc, getDocs, query, setDoc, updateDoc, where } from "firebase/firestore/lite";
 import { CircularProgress } from '@mui/material';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginSignupComponent = () => {
+
+    const accountsCollection = collection(db, "accounts");
+
+
 
     const [logInLoading, setlogInLoading] = useState(false)
     const [signUpLoading, setsignUpLoading] = useState(false)
@@ -32,6 +35,10 @@ const LoginSignupComponent = () => {
     const [aboutBrand, setAboutBrand] = useState('');
     const [userType, setUserType] = useState('brand');
     const [previewPhoto, setPreviewPhoto] = useState(null);
+    const [username, setusername] = useState('');
+    const [brandName, setbrandName] = useState('');
+    const [userNameErrorMessage, setuserNameErrorMessage] = useState(['',''])
+    const [errorMessage, seterrorMessage] = useState('')
 
     
 
@@ -41,6 +48,58 @@ const LoginSignupComponent = () => {
         setPreviewPhoto(URL.createObjectURL(file));
     };
 
+    const checkUniqueUserName = async (e)=>{
+        const val = e.target.value
+        setusername(val.trim())
+        if(val.length==0) {
+            setuserNameErrorMessage(['','']); 
+            return;
+        }
+        try {
+            const q = query(accountsCollection, where("username", "==", val));
+            const querySnapshot = await getDocs(q);
+            const exists = !querySnapshot.empty;
+        
+            if (exists) {
+              console.log("Username exists!");
+              setuserNameErrorMessage(['red','Username already exists'])
+            } else {
+                
+              setuserNameErrorMessage(['green','Username is unique'])
+            }
+        
+            return exists;
+          } catch (error) {
+            console.error("Error checking username:", error);
+            return false;
+          }
+    }
+
+
+    const firebaseUpdateForSignUp = async ()=>{
+        try{
+            const docData = {
+                accountType: userType === "brand" ? "brand" : "user",
+                accountRef: "", // Placeholder for document ID, will be updated later
+                brandInfo: userType === "brand" ? {
+                  aboutText: aboutBrand,
+                  brandCover: "2.jpg",
+                  brandTitle: brandName,
+                  ratings: []
+                } : {},
+                email: email,
+                profilePic: userType !== "brand" ? "avatar.png" : "",
+                username: userType !== "brand" ? username: ""
+              };
+              const docRef = await addDoc(accountsCollection, docData);
+              docData.accountRef = docRef.id;
+              updateDoc(doc(db,"accounts",docRef.id),{"accountRef":docRef.id})
+        }
+        catch(e){
+            alert(e.code)
+        }
+        
+    }
 
 
     const loginUser = async () => {
@@ -78,6 +137,55 @@ const LoginSignupComponent = () => {
         
         setlogInLoading(false)
       };
+
+
+    const signUpUserOrBrand = async()=>{
+        if(password != confirmPassword) return;
+        try {
+            const { user } = await createUserWithEmailAndPassword(auth, email, password);
+            firebaseUpdateForSignUp()
+            try {
+                await sendEmailVerification(user);
+                setEmail('')
+                setPassword('')
+                setConfirmPassword('')
+                setusername('')
+                setbrandName('')
+                alert('Email verification link sent successfully');
+            }
+            catch(e){
+                alert("Something went wrong")
+                console.log(e)
+            }
+          } 
+          catch (e) {
+            if(e.code==='auth/email-already-in-use') seterrorMessage("Email has already been used")
+            else if(e.code==='auth/weak-password') seterrorMessage("Password provide a strong password")
+            else if(e.code === 'auth/invalid-email') seterrorMessage("Please provide a valid email")
+            alert(errorMessage)
+          }
+    }
+
+    const onSingUpPress = async ()=>{
+        setsignUpLoading(true)
+        if(email.length==0 || password.length == 0){
+            seterrorMessage("Please provide all the necessarry informations")
+            alert(errorMessage)
+        }
+        else if(email.length>0 && password.length>0 && confirmPassword.length>0) {
+            if(password===confirmPassword) signUpUserOrBrand()
+            else if(password!==confirmPassword) {
+                seterrorMessage("Passwords do not match");
+                alert(errorMessage)
+            }
+            // else seterrorMessage("Please provide a valid username")
+        }
+        else{
+            seterrorMessage("Something is missing!")
+            alert(errorMessage)
+        }
+        setsignUpLoading(false)
+    }
     
 
     useEffect(() => {
@@ -123,48 +231,6 @@ const LoginSignupComponent = () => {
                     <Typography variant="h4" color="#FF5722" gutterBottom>
                         Sign Up
                     </Typography>
-                    <TextField
-                        id="email"
-                        label="Email"
-                        type="email"
-                        fullWidth
-                        variant="outlined"
-                        margin="normal"
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}
-                    />
-                    <TextField id="password" label="Password" type="password" fullWidth variant="outlined"
-                        margin="normal" value={password}  onChange={(e)=>setPassword(e.target.value)}
-                    />
-                    <TextField
-                        id="confirmPassword" label="Confirm Password" type="password" fullWidth variant="outlined"
-                        margin="normal" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}
-                    />
-                    {userType === 'brand' && (
-                        <>
-                        <input
-                            accept="image/*"
-                            id="coverPhoto"
-                            type="file"
-                            onChange={handleCoverPhotoChange}
-                            style={{ display: 'none' }}
-                        />
-                        <label htmlFor="coverPhoto">
-                            <Button variant="contained" component="span" fullWidth>
-                            {coverPhoto ? 'Change Image' : 'Upload Image'}
-                            </Button>
-                        </label>
-                        {previewPhoto && (
-                            <img src={previewPhoto} alt="Uploaded Cover" style={{ width: '50%', marginTop: '16px',margin:'auto' }} />
-                        )}
-                        </>
-                    )}
-                    
-                    <TextField id="aboutBrand" label="About Brand" type="text" fullWidth variant="outlined"
-                        margin="normal"  value={aboutBrand}
-                        onChange={(e)=>setAboutBrand(e.target.value)}
-                        style={{ display: userType === 'customer' ? 'none' : 'block' }}
-                    />
                     <FormControl component="fieldset" style={{ marginTop: '16px' }}>
                         <RadioGroup row value={userType} onChange={(e) => setUserType(e.target.value)}>
                             <FormControlLabel
@@ -180,12 +246,61 @@ const LoginSignupComponent = () => {
                                 sx={{ color: 'black', fontWeight: 500 }}
                             />
                         </RadioGroup>
-
                     </FormControl>
+                    <TextField id="email" label="Email" type="email" fullWidth variant="outlined" margin="normal"
+                        value={email} onChange={(e)=>setEmail(e.target.value)}
+                    />
+                    <TextField id="password" label="Password" type="password" fullWidth variant="outlined"
+                        margin="normal" value={password}  onChange={(e)=>setPassword(e.target.value)}
+                    />
+                    <TextField
+                        id="confirmPassword" label="Confirm Password" type="password" fullWidth variant="outlined"
+                        margin="normal" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}
+                    />
+                    {userType === 'brand'?
+
+                        <TextField
+                            id="brandName" label="Brand name" type="text" fullWidth variant="outlined"
+                            margin="normal" value={brandName} onChange={(e)=>setbrandName(e.target.value)}
+                        />:
+                        <TextField
+                            id="userName" label="Username" type="text" fullWidth variant="outlined"
+                            margin="normal" value={username} onChange={(e)=>checkUniqueUserName(e)}
+                        />
+                    }
+                    {
+                        userType !== 'brand' && userNameErrorMessage[1].length>0 && <span style={{color:userNameErrorMessage[0]}}>{userNameErrorMessage[1]}</span>
+                    }
+                    {userType === 'brand' && (
+                        <>
+                            <input
+                                accept="image/*"
+                                id="coverPhoto"
+                                type="file"
+                                onChange={handleCoverPhotoChange}
+                                style={{ display: 'none' }}
+                            />
+                            <label htmlFor="coverPhoto">
+                                <Button variant="contained" component="span" fullWidth>
+                                {coverPhoto ? 'Change Image' : 'Upload Image'}
+                                </Button>
+                            </label>
+                            {previewPhoto && (
+                                <img src={previewPhoto} alt="Uploaded Cover" style={{ width: '50%', marginTop: '16px',margin:'auto' }} />
+                            )}
+                        </>
+                    )}
+                    
+                    <TextField id="aboutBrand" label="About Brand" type="text" fullWidth variant="outlined"
+                        margin="normal"  value={aboutBrand} multiline={true}
+                        onChange={(e)=>setAboutBrand(e.target.value)}
+                        style={{ display: userType === 'customer' ? 'none' : 'block' }}
+                    />
+                    
                     <Box fullWidth style={{display:'flex',justifyContent:'center',marginTop:'20px'}}>
-                        <Button style={{backgroundColor:'#FF5722',color:'white',width:'60%',margin:'auto',height:'45px'}} onClick={()=>setlogInLoading(true)}>
+                        <Button style={{backgroundColor:'#FF5722',color:'white',width:'60%',margin:'auto',height:'45px'}} onClick={()=>onSingUpPress()}>
                         {
-                            signUpLoading?<CircularProgress size={20} style={{color:'#fff'}}/>:"Log In"
+                            signUpLoading?<CircularProgress size={20} style={{color:'#fff'}}/>:"Sign Up"
                         }
                         </Button>
                     </Box>
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getStorage} from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +23,8 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export const storage = getStorage();
 
 export const db = getFirestore(app)
 export const auth = getAuth();
